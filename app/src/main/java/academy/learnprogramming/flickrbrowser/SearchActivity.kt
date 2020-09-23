8af0d550c7ddf07092c601fd40110291c73c1b8e
unknown <andrija.m.nikic@gmail.com>
2020-09-23 17:34:30 +0200

    FlickrBrowser app

---
+++ app/src/main/java/academy/learnprogramming/flickrbrowser/SearchActivity.kt
@@ -0,0 +1,71 @@
+package academy.learnprogramming.flickrbrowser
+
+import android.app.SearchManager
+import android.content.Context
+import android.os.Bundle
+import android.preference.PreferenceManager
+import android.util.Log
+import android.view.Menu
+import android.widget.SearchView
+
+class SearchActivity : BaseActivity() {
+    private val TAG = "SearchActivity"
+    private var searchView: SearchView? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        Log.d(TAG,".onCreate: starts")
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search)
+        activateToolBar(true)
+        Log.d(TAG, ".onCreate: ends")
+//        setSupportActionBar(toolbar)
+
+//        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        Log.d(TAG, ".onCreateOptionsMenu: starts")
+        menuInflater.inflate(R.menu.menu_search, menu)
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        searchView = menu.findItem(R.id.app_bar_search).actionView as SearchView
+        val searchableInfo = searchManager.getSearchableInfo(componentName)
+        searchView?.setSearchableInfo(searchableInfo)
+
+//        Log.d(TAG,".onCreateOptionsMenu: $componentName")
+//        Log.d(TAG,".onCreateOptionsMenu: hint is ${searchView?.queryHint}")
+//        Log.d(TAG,".onCreateOptionsMenu: $searchableInfo")
+
+        searchView?.isIconified = false
+
+        searchView?.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d(TAG, ".onQueryTextSubmit: called")
+
+                val sharePref = PreferenceManager.getDefaultSharedPreferences(applicationContext)
+                sharePref.edit().putString(FLICKR_QUERY, query).apply()
+                searchView?.clearFocus()
+
+                finish()
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                return false
+            }
+        })
+
+        searchView?.setOnCloseListener {
+            finish()
+            false
+        }
+
+        Log.d(TAG,".onCreateOptionsMenu: returning")
+        return true
+    }
+
+    override fun onResume() {
+        Log.d(TAG,".onResume starts")
+        super.onResume()
+    }
+}
